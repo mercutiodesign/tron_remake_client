28b1bc360c8e14aaa5be8435f1830bad9d609423
Martin Dreher <luma.inc@gmail.com>
2018-05-10 22:33:14 +0200

    basic directory structure and webpack environments

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        title: 'Tron Remake',
+        meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
